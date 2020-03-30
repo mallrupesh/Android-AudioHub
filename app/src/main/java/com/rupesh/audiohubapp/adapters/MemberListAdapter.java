5c389f715d0364f1c236f58aac05d61594953559
Rupak80 <rm19adm@herts.ac.uk>
2020-03-30 16:51:26 +0100

    33 handled Firebase asynchronous method calls. Member names are loaded in the recycler view.

--- app/src/main/java/com/rupesh/audiohubapp/adapters/MemberListAdapter.java
+++ app/src/main/java/com/rupesh/audiohubapp/adapters/MemberListAdapter.java
@@ -1,5 +1,6 @@
 package com.rupesh.audiohubapp.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,64 +9,50 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.rupesh.audiohubapp.R;
-import com.rupesh.audiohubapp.model.Project;
-import com.rupesh.audiohubapp.model.ProjectMember;
+import com.rupesh.audiohubapp.model.User;
 
-public class MemberListAdapter extends FirebaseRecyclerAdapter<ProjectMember, MemberListAdapter.MemberListViewHolder> {
+import java.util.ArrayList;
 
-    DatabaseReference memberDatabaseRef;
-    private Project project;
-    /**
-     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
-     * {@link FirebaseRecyclerOptions} for configuration options.
-     *
-     * @param options
-     */
-    public MemberListAdapter(@NonNull FirebaseRecyclerOptions<ProjectMember> options, Project project) {
-        super(options);
-        this.project = project;
-        memberDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Project_Members").child(project.getProjectId());
-    }
+public class MemberListAdapter extends RecyclerView.Adapter<MemberListAdapter.MemberViewHolder> {
 
-    @Override
-    protected void onBindViewHolder(@NonNull final MemberListViewHolder holder, int position, @NonNull final ProjectMember model) {
+    private ArrayList<User> users;
+    Context context;
 
-       /* memberDatabaseRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.getChildren().toString().trim();
-                holder.memberName.setText(memberDatabaseRef.child(name).getKey());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });*/
-
-
+    public MemberListAdapter(ArrayList<User> users, Context context) {
+        this.users = users;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public MemberListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MemberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.layout_members_row_item, parent, false);
-        return new MemberListViewHolder(view);
+        return new MemberViewHolder(view);
     }
 
-    class MemberListViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public void onBindViewHolder(@NonNull MemberViewHolder holder, int position) {
+        holder.memberName.setText(users.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        // For Firebase asynchronous recall
+        if (users == null)
+            return 0;
+        return users.size();
+    }
+
+
+    public class MemberViewHolder extends RecyclerView.ViewHolder{
 
         TextView memberName;
 
-        public MemberListViewHolder(@NonNull View itemView) {
+        public MemberViewHolder(@NonNull View itemView) {
             super(itemView);
-
             memberName = itemView.findViewById(R.id.textViewMembersName);
         }
     }
