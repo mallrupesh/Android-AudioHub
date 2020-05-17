ed412265d05dd653883e926b14a90a31c838b846
Rupak80 <rm19adm@herts.ac.uk>
2020-05-17 13:08:52 +0100

    42th commit. Strengthened the password validation for Registration and Login using regex.

--- app/src/main/java/com/rupesh/audiohubapp/model/User.java
+++ app/src/main/java/com/rupesh/audiohubapp/model/User.java
@@ -4,6 +4,8 @@ import android.text.TextUtils;
 import android.util.Patterns;
 
 import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class User implements IUser, Serializable {
 
@@ -15,24 +17,45 @@ public class User implements IUser, Serializable {
     private String createdOn;
     private String uid;
 
-
     /**
      * Empty constructor for Firebase operation
      */
     public User(){}
 
 
+    /**
+     * Used for Login process
+     * @param email
+     * @param password
+     */
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * Used for Registration process
+     * @param name
+     * @param email
+     * @param password
+     */
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 
+    /**
+     * Used for application operations
+     * @param name
+     * @param email
+     * @param image
+     * @param status
+     * @param password
+     * @param createdOn
+     * @param uid
+     */
     public User(String name, String email, String image, String status, String password, String createdOn, String uid) {
         this.name = name;
         this.email = email;
@@ -65,25 +88,50 @@ public class User implements IUser, Serializable {
     /**
      * Validates the User's name, email, and password before registering the User to
      * the app.
-     * @return
+     * @return true if email and password match the given patterns
+     *         false if email and password do not match the given patterns
      */
     @Override
     public boolean isValidRegisteredData() {
         return !TextUtils.isEmpty(getEmail())
                 && !TextUtils.isEmpty(getName())
                 && Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches()
-                && getPassword().length() > 6;
+                && validateUserPassword();
     }
 
     /**
      * Validates the User's email and password each time before the User login the app
-     * @return
+     * @return true if email and password match the given patterns
+     *         false if email and password do not match the given patterns
      */
     @Override
     public boolean isValidLoginData() {
         return !TextUtils.isEmpty(getEmail())
                 && Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches()
-                && getPassword().length() > 6;
+                && validateUserPassword();
+    }
+
+
+    /**
+     * Validate the User's password. Uses regex to match the password pattern
+     * 1. (?=.*[0-9]) -> password must contain at least 1 digit
+     * 2. (?=.*[a-z]) -> password must contain at least 1 lower case letter
+     * 3. (?=.*[A-Z]) -> password must contain at least 1 upper case letter
+     * 4. (?=.*[@#$%^&+=]) -> password must contain at least 1 special character
+     * 5. (?=\S+$) -> password must not have whitespaces in the entire string
+     * 6. .{8,} -> password must be at least 8 places long
+     * @return true if password adheres the above conditions
+     *         false if password fails to adhere the above conditions.
+     */
+    public boolean validateUserPassword() {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_SEQUENCE = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+        pattern = Pattern.compile(PASSWORD_SEQUENCE);
+        matcher = pattern.matcher(getPassword());
+
+        return matcher.matches();
     }
 
     public String getCreatedOn() {
@@ -109,7 +157,6 @@ public class User implements IUser, Serializable {
     public void setStatus(String status) {
         this.status = status;
     }
-
 
     public String getUid() {
         return uid;
