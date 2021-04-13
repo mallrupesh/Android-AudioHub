a8688ba12b03590cd7723480f530335581bff013
mallrupesh <72442352+mallrupesh@users.noreply.github.com>
2021-04-13 03:20:17 +0100

    Delete AndroidStudioProjects/AudioHubApp/app directory

--- AndroidStudioProjects/AudioHubApp/app/src/main/java/com/rupesh/audiohubapp/start/IPresenterProtocol.java
+++
@@ -1,7 +0,0 @@
-package com.rupesh.audiohubapp.view;
-
-public interface IPresenterProtocol {
-    void onLogin(String email, String password);
-    void onRegister(String name, String email, String password);
-}
-
